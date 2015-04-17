21352b7824b9fe26174040ad4887bda08f46f617
Andresteve07 <andresteve07@gmail.com>
2015-04-17 15:59:17 -0300

    Agregada logica de decision de salto.

---
+++ modulos/instruction_decode/branch_logic.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:33:54 04/17/2015 
+// Design Name: 
+// Module Name:    branch_logic 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module branch_logic(
+							input [1:0] DE_control,
+							input cmp,
+							output reg PC_sel
+							);
+
+assign PC_sel = ((DE_control[0] ~^ cmp) & DE_control[1]);
+endmodule
---
+++ modulos/instruction_decode/comparer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    15:00:21 04/17/2015 
+// Design Name: 
+// Module Name:    suber 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module comparer(a,b,cmp_out);
+parameter BUS_SIZE = 10;
+input [BUS_SIZE-1:0] a,b;
+output sub;
+assign sub = (a == b);
+endmodule
